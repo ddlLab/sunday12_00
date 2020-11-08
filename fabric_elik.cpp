9ce0bd670b565d26f0fa396652728888a6b795c9
Amarinhobest <tama1611201187@gmail.com>
2020-11-08 13:16:07 +0200

    elik

--- fabric_elik.cpp
+++ fabric_elik.cpp
@@ -1,1 +1,21 @@
+#include "fabric_elik.h"
+#include "elix.h"
 
+eFabricElik::eFabricElik(
+                float _probabilityHeal,
+                float _probabilityMana,
+                float _probabilityExp)
+: probabilityHeal(_probabilityHeal)
+, probabilityMana(_probabilityMana)
+, probabilityExp(_probabilityExp)
+{}
+
+eElik* eFabricElik::Make() const
+{
+    float t = probabilityHeal;
+    float r = (rand()%101)/100.0;
+    if(r < t)                        return new eHealElik();
+    t += probabilityMana;
+    if(r < t)                        return new eManaElik();
+    return new eExpElik();
+}
